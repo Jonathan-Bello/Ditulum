ac8838a2e0ee68ee118b7efdeafdee8bede10493
Jonathan Bello <jonathan.bellofg@gmail.com>
2023-10-27 04:32:49 -0600

    agregada hojita

--- src/components/pages/home/HomeServiciosSection.jsx
+++ src/components/pages/home/HomeServiciosSection.jsx
@@ -4,6 +4,7 @@ import servitapiceria from "../../../assets/img/home/servitapiceria.jpg"
 import servijardineria from "../../../assets/img/home/servijardineria.jpg"
 import serviflores from "../../../assets/img/home/serviflores.jpg"
 import serviProx from "../../../assets/img/home/serviProx.jpg"
+import hojaSvg from "../../../assets/img/home/hoja.svg"
 import CardServiHome from "./CardServiHome"
 
 const HomeServiciosSection = () => {
@@ -42,6 +43,8 @@ const HomeServiciosSection = () => {
           url={"#"}
         />
       </div>
+
+      <img className="HomeServiciosSection__hoja" src={hojaSvg} alt="hojita" />
     </section>
   )
 }
