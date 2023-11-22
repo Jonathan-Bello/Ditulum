91ff4815a48bb01e112a6d91fc437430e326cae5
Jonathan Bello <jonathan.bellofg@gmail.com>
2023-11-22 14:06:33 -0600

    agregado página provicional de catalogo

---
+++ src/components/pages/catalogo/CatalogoContent.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const CatalogoContent = () => {
+  return (
+    <div className="CatalogoContent">
+      <p
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Para conocer nuestro catálogo, contactate con nosotros por <br />
+        <a
+          href="https://api.whatsapp.com/send?phone=5219841528989"
+          target="_blank"
+        >
+          Whatsapp 984 152 8989
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default CatalogoContent
---
+++ src/pages/catalogo.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/Layout"
+import CatalogoContent from "../components/pages/catalogo/CatalogoContent"
+
+
+const index = () => {
+  return (
+    <Layout>
+      <CatalogoContent />
+    </Layout>
+  )
+}
+
+export default index
