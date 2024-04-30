19aa9c74ee002c6b624301586db0d0a164f71699
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-04-30 11:20:35 -0600

    edit site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Ditulum Site`,
+    description: `Creamos experiencias que te permiten desarrollarte de manera funcional y creativa, sin dejar de lado un ambiente natural y saludable.`,
+    author: `Jonathan Alexis Bello Lopez`,
+    siteUrl: `https://ditulum.com/`,
   },
   plugins: [
     "gatsby-plugin-sass",
